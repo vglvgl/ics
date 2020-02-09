be47e21b27fc621472499a97b12fadb6c60828c3
vglvgl <vglvgl@users.noreply.github.com>
2020-02-09 12:39:02 +0100

    Add files via upload

---
+++ ics.js
@@ -0,0 +1,33 @@
+var url = 'https://ics-api.herokuapp.com/ics';
+var id = 1;
+
+$().ready(function() {
+    axios.get(`${url}/${id}`)
+        .then ( response => {
+            load(response.data)
+        } ).catch( err => {
+            console.log(err)
+        } );
+});
+
+function get_info(num){
+    axios.get(`${url}/${num}`)
+    .then ( response => {
+        load(response.data)
+    } ).catch( err => {
+        console.log(err)
+    } );
+}
+
+
+
+function load(response){
+    $( '.char' ).text(response.char);
+    $( '.name' ).text(response.name);
+    $( '.pron' ).text(response.pronunciation);
+    $( '.meaning' ).text(response.meaning);
+    $( '.flag' ).html('<img src="'+response.flag+'" alt="'+response.name+'" height="100" width="100">');
+    $( '.morse' ).text(response.morse);
+    if (response.audio != "") $( '.audio' ).html('<audio controls><source src="'+response.audio+'" type="audio/mpeg"></audio>');
+    else $( '.audio' ).html('');
+}
